db5373d054dcbff6e7185751b8593a777af07bd6
anthonywittemann <awittema@usc.edu>
2015-08-06 16:36:58 -0700

    needed to use HAVING instead of where

--- ERAvsERPercentage.sql
+++ ERAvsERPercentage.sql
@@ -2,7 +2,8 @@ SELECT playerID,
         9 * SUM(ER) / (SUM(IPOuts) / 3) as ERA,
         9 * SUM(R) / (SUM(IPOuts) / 3) as RA9,
         SUM(ER) / SUM(R) as ERPercentage
-FROM Pitching
-WHERE SUM(IPOuts) >= 3000 AND yearID >= 2000 AND yearID <= 2009
+FROM Pitching p
+WHERE yearID >= 2000 AND yearID <= 2009
 GROUP BY playerID
+HAVING SUM(IPOuts) >= 3000
 ORDER BY ERPercentage, playerID
